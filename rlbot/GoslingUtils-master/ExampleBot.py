cab7a0e5079fa41a28aac940f55d983b1a9fa0d8
pedrogsfernandes <goncafer@sapo.pt>
2021-05-18 15:17:04 +0100

    Bot now shoots if he has a chance

--- rlbot/GoslingUtils-master/ExampleBot.py
+++ rlbot/GoslingUtils-master/ExampleBot.py
@@ -7,16 +7,23 @@ from routines import *
 
 class ExampleBot(GoslingAgent):
     def run(agent):
+    	if agent.team == 0:
+    		agent.debug_stack()
+
         #An example of using raw utilities:
-        relative_target = agent.ball.location - agent.me.location
-        local_target = agent.me.local(relative_target)
-        defaultPD(agent, local_target)
-        defaultThrottle(agent, 2300)
+        #relative_target = agent.ball.location - agent.me.location
+        #local_target = agent.me.local(relative_target)
+        #defaultPD(agent, local_target)
+        #defaultThrottle(agent, 2300)
 
         #An example of pushing routines to the stack:
+        targets = {"goal":(agent.foe_goal.left_post,agent.foe_goal.right_post)}
+        shots = find_hits(agent,targets)
         if len(agent.stack) < 1:
             if agent.kickoff_flag:
                 agent.push(kickoff())
+            elif len(shots["goal"]) > 0:
+            	agent.push(shots["goal"][0])
             else:
-                agent.push(atba())
+            	agent.push(atba())
 
