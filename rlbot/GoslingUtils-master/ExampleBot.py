7aedb357d7f128e8183f7fe75bd0f0c4477e3aa5
pedrogsfernandes <goncafer@sapo.pt>
2021-05-18 15:37:50 +0100

    Fixed stack debug not showing

--- rlbot/GoslingUtils-master/ExampleBot.py
+++ rlbot/GoslingUtils-master/ExampleBot.py
@@ -1,29 +1,27 @@
-from tools import  *
+from tools import *
 from objects import *
 from routines import *
 
 
-#This file is for strategy
+# This file is for strategy
 
 class ExampleBot(GoslingAgent):
     def run(agent):
-    	if agent.team == 0:
-    		agent.debug_stack()
+        agent.debug_stack()
 
-        #An example of using raw utilities:
-        #relative_target = agent.ball.location - agent.me.location
-        #local_target = agent.me.local(relative_target)
-        #defaultPD(agent, local_target)
-        #defaultThrottle(agent, 2300)
+        # An example of using raw utilities:
+        # relative_target = agent.ball.location - agent.me.location
+        # local_target = agent.me.local(relative_target)
+        # defaultPD(agent, local_target)
+        # defaultThrottle(agent, 2300)
 
-        #An example of pushing routines to the stack:
-        targets = {"goal":(agent.foe_goal.left_post,agent.foe_goal.right_post)}
-        shots = find_hits(agent,targets)
+        # An example of pushing routines to the stack:
+        targets = {"goal": (agent.foe_goal.left_post, agent.foe_goal.right_post)}
+        shots = find_hits(agent, targets)
         if len(agent.stack) < 1:
             if agent.kickoff_flag:
                 agent.push(kickoff())
             elif len(shots["goal"]) > 0:
-            	agent.push(shots["goal"][0])
+                agent.push(shots["goal"][0])
             else:
-            	agent.push(atba())
-
+                agent.push(atba())
